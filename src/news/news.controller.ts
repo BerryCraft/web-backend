b1ffc4be853d19c9869b45f44419bd35bd1e705a
cofeek-codes <kormyshev11@mail.ru>
2023-04-21 23:44:32 +0500

    auth guards implemented and tested

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { Auth } from 'src/decorators/auth.decorator'
 import { NewsDTO } from './dto/news.dto'
 import { NewsService } from './news.service'
 
@@ -10,14 +11,19 @@ export class NewsController {
 	getAll() {
 		return this.newsService.getAll()
 	}
+
+	@Auth()
 	@Post('new')
 	create(@Body() dto: NewsDTO) {
 		return this.newsService.create(dto)
 	}
+
+	@Auth()
 	@Put('/e/:id')
 	update(@Param('id') id: string, @Body() dto: NewsDTO) {
 		return this.newsService.update(id, dto)
 	}
+	@Auth()
 	@Delete('/d/:id')
 	delete(@Param('id') id: string) {
 		return this.newsService.delete(id)
